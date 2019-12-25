7bc8e52ff4c9417eb014dc039b89317fd028b2d3
Niroj Shrestha <nirojshrestha019@gmail.com>
2019-12-25 11:12:58 -0400

    Update static.py

--- core/config/static.py
+++ core/config/static.py
@@ -1,11 +1,6 @@
 
-# uv_skin
-# username = r'feeds@exclusiveconcepts.com'
-# encrypted_password = r'94FA23658E5F0B8F81BA5F5EF83CBBFDDA8A8EC6ADBC219A0FC8526107E212F3'
-# link = r'https://www.uvskinz.com/'
-
 # shopmrt
-username = r'exclusive.mrt@gmail.com'
-encrypted_password = r'024F932D309E694E893FA52F5D168CA8A03122D3E8C252B936F56F357315B5F0'
-link = r'https://www.shopmrt.com/'
+username = r'example@gmail.com'
+encrypted_password = r'example_encrypted_password'
+link = r'https://www.link.com/'
 
